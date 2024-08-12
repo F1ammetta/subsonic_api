5a4cf8777148d0dd95fc08a8a3ee7e1915997fb7
Sam-H420 <sherazo@utb.edu.co>
2024-08-12 10:21:00 -0500

    added custom toString for SubsonicClient

--- example/subsonic_api_example.dart
+++ example/subsonic_api_example.dart
@@ -1,12 +1,13 @@
 import 'package:subsonic_api/subsonic_api.dart';
 
 void main() async {
-  String salt = SubSonicClient.createSalt();
-  String token = SubSonicClient.createToken(salt, 'password');
+  String salt = createSalt();
+  String token = createToken(salt, 'password');
   SubSonicClient client = SubSonicClient('http://localhost:4040', 'username',
       token, salt, 'SubSonicApiExample', '0.0.1');
 
   var response = await client.ping();
 
   print(response.status);
+  print(client.toString());
 }
